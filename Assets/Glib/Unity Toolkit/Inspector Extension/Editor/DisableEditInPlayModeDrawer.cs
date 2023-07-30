04a56a9281422e2133c1863195687e0e82e52686
Harumi Maruoka <106292983+HarumiMaruoka@users.noreply.github.com>
2023-07-30 16:17:23 +0900

    エディタ上でゲーム再生中はインスペクタウィンドウから値の変更をできないようにする属性を作成

---
+++ Assets/Glib/Unity Toolkit/Inspector Extension/Editor/DisableEditInPlayModeDrawer.cs
@@ -0,0 +1,25 @@
+// 日本語対応
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(DisableEditInPlayModeAttribute))]
+public class DisableEditInPlayModeDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        // ゲームが再生中でない場合のみ、通常のフィールドを表示
+        if (!EditorApplication.isPlaying)
+        {
+            EditorGUI.PropertyField(position, property, label);
+        }
+        else
+        {
+            // ゲームが再生中の場合、フィールドを無効化（編集不可）にする
+            GUI.enabled = false;
+            EditorGUI.PropertyField(position, property, label);
+            GUI.enabled = true;
+        }
+    }
+}
+#endif
